fbc5c9a6fc2fffa3ec882eb1202ce5d02707ab22
miller <miller.lmf@gmail.com>
2017-05-06 09:29:31 +0800

    no message

--- js/main.js
+++ js/main.js
@@ -1,23 +1,18 @@
-jQuery(function($) {
+jQuery(function ($) {
 
     var BRUSHED = window.BRUSHED || {};
 
     /* ==================================================
-       Nav bar control
-       ================================================== */
+     Nav bar control
+     ================================================== */
     var $lateral_menu_trigger = $('#cd-menu-trigger');
     //open-close lateral menu clicking on the menu icon
-    $lateral_menu_trigger.on('click', function(event) {
+    $lateral_menu_trigger.on('click', function (event) {
         event.preventDefault();
 
         $lateral_menu_trigger.toggleClass('is-clicked');
         $navigation.toggleClass('lateral-menu-is-open');
-        /*$content_wrapper.toggleClass('lateral-menu-is-open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-        	// firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
-        	$('body').toggleClass('overflow-hidden');
-        });*/
         $('#cd-lateral-nav').toggleClass('lateral-menu-is-open');
-
         //check if transitions are not supported - i.e. in IE9
         if ($('html').hasClass('no-csstransitions')) {
             $('body').toggleClass('overflow-hidden');
@@ -28,18 +23,18 @@ jQuery(function($) {
     // console.log($('.item-has-children').children('a')[0]);
     $('#defualtTag').addClass('submenu-open');
     $('#defualtUl').show();
-    $('.item-has-children').children('a').on('click', function(event) {
+    $('.item-has-children').children('a').on('click', function (event) {
         event.preventDefault();
         $(this).toggleClass('submenu-open').next('.sub-menu').slideToggle(200).end().parent('.item-has-children').siblings('.item-has-children').children('a').removeClass('submenu-open').next('.sub-menu').slideUp(200);
     });
 
 
     /* ==================================================
-       Mobile Navigation
-       ================================================== */
+     Mobile Navigation
+     ================================================== */
     var mobileMenuClone = $('#menu').clone().attr('id', 'navigation-mobile');
 
-    BRUSHED.mobileNav = function() {
+    BRUSHED.mobileNav = function () {
         var windowWidth = $(window).width();
 
         if (windowWidth <= 979) {
@@ -55,8 +50,8 @@ jQuery(function($) {
         }
     }
 
-    BRUSHED.listenerMenu = function() {
-        $('#mobile-nav').on('click', function(e) {
+    BRUSHED.listenerMenu = function () {
+        $('#mobile-nav').on('click', function (e) {
             $(this).toggleClass('open');
 
             if ($('#mobile-nav').hasClass('open')) {
@@ -67,7 +62,7 @@ jQuery(function($) {
             e.preventDefault();
         });
 
-        $('#menu-nav-mobile a').on('click', function() {
+        $('#menu-nav-mobile a').on('click', function () {
             $('#mobile-nav').removeClass('open');
             $('#navigation-mobile').slideUp(350, 'easeOutExpo');
         });
@@ -75,10 +70,10 @@ jQuery(function($) {
 
 
     /* ==================================================
-       Slider Options
-       ================================================== */
+     Slider Options
+     ================================================== */
 
-    BRUSHED.slider = function() {
+    BRUSHED.slider = function () {
         $.supersized({
             // Functionality
             slideshow: 1, // Slideshow on/off
@@ -115,7 +110,7 @@ jQuery(function($) {
                     thumb: '',
                     url: ''
                 }, {
-                    image: 'img/slider-images/image05.jpg',
+                    image: 'img/slider-images/image02.jpg',
                     title: '<div class="slide-content"></div>',
                     thumb: '',
                     url: ''
@@ -142,26 +137,23 @@ jQuery(function($) {
 
 
     /* ==================================================
-       Navigation Fix
-       ================================================== */
+     Navigation Fix
+     ================================================== */
 
-    BRUSHED.nav = function() {
+    BRUSHED.nav = function () {
         $('.sticky-nav').waypoint('sticky');
     }
 
-
-
-
     /* ==================================================
-       FancyBox
-       ================================================== */
+     FancyBox
+     ================================================== */
 
-    BRUSHED.fancyBox = function() {
+    BRUSHED.fancyBox = function () {
         if ($('.fancybox').length > 0 || $('.fancybox-media').length > 0 || $('.fancybox-various').length > 0) {
 
             $(".fancybox").fancybox({
                 padding: 0,
-                beforeShow: function() {
+                beforeShow: function () {
                     this.title = $(this.element).attr('title');
                     this.title = '<h4>' + this.title + '</h4>' + '<p>' + $(this.element).parent().find('img').attr('alt') + '</p>';
                 },
@@ -184,11 +176,11 @@ jQuery(function($) {
 
 
     /* ==================================================
-       Contact Form
-       ================================================== */
+     Contact Form
+     ================================================== */
 
-    BRUSHED.contactForm = function() {
-        $("#contact-submit").on('click', function() {
+    BRUSHED.contactForm = function () {
+        $("#contact-submit").on('click', function () {
             $contact_form = $('#contact-form');
 
             var fields = $contact_form.serialize();
@@ -198,7 +190,7 @@ jQuery(function($) {
                 url: "php/contact.php",
                 data: fields,
                 dataType: 'json',
-                success: function(response) {
+                success: function (response) {
 
                     if (response.status) {
                         $('#contact-form input').val('');
@@ -213,12 +205,11 @@ jQuery(function($) {
     }
 
 
-
     /* ==================================================
-       Menu Highlight
-       ================================================== */
+     Menu Highlight
+     ================================================== */
 
-    BRUSHED.menu = function() {
+    BRUSHED.menu = function () {
         $('#menu-nav, #menu-nav-mobile').onePageNav({
             currentClass: 'current',
             changeHash: false,
@@ -231,11 +222,11 @@ jQuery(function($) {
     }
 
     /* ==================================================
-       Next Section
-       ================================================== */
+     Next Section
+     ================================================== */
 
-    BRUSHED.goSection = function() {
-        $('#nextsection').on('click', function() {
+    BRUSHED.goSection = function () {
+        $('#nextsection').on('click', function () {
             $target = $($(this).attr('href')).offset().top - 30;
 
             $('body, html').animate({
@@ -246,11 +237,11 @@ jQuery(function($) {
     }
 
     /* ==================================================
-       GoUp
-       ================================================== */
+     GoUp
+     ================================================== */
 
-    BRUSHED.goUp = function() {
-        $('#goUp').on('click', function() {
+    BRUSHED.goUp = function () {
+        $('#goUp').on('click', function () {
             $target = $($(this).attr('href')).offset().top - 30;
 
             $('body, html').animate({
@@ -262,27 +253,27 @@ jQuery(function($) {
 
 
     /* ==================================================
-    	Scroll to Top
-    	================================================== */
+     Scroll to Top
+     ================================================== */
 
-    BRUSHED.scrollToTop = function() {
+    BRUSHED.scrollToTop = function () {
         var windowWidth = $(window).width(),
             didScroll = false;
 
         var $arrow = $('#back-to-top');
 
-        $arrow.click(function(e) {
+        $arrow.click(function (e) {
             $('body,html').animate({
                 scrollTop: "0"
             }, 750, 'easeOutExpo');
             e.preventDefault();
         })
 
-        $(window).scroll(function() {
+        $(window).scroll(function () {
             didScroll = true;
         });
 
-        setInterval(function() {
+        setInterval(function () {
             if (didScroll) {
                 didScroll = false;
 
@@ -296,22 +287,22 @@ jQuery(function($) {
     }
 
     /* ==================================================
-       Thumbs / Social Effects
-       ================================================== */
+     Thumbs / Social Effects
+     ================================================== */
 
-    BRUSHED.utils = function() {
+    BRUSHED.utils = function () {
 
-        $('.item-thumbs').bind('touchstart', function() {
+        $('.item-thumbs').bind('touchstart', function () {
             $(".active").removeClass("active");
             $(this).addClass('active');
         });
 
-        $('.image-wrap').bind('touchstart', function() {
+        $('.image-wrap').bind('touchstart', function () {
             $(".active").removeClass("active");
             $(this).addClass('active');
         });
 
-        $('#social ul li').bind('touchstart', function() {
+        $('#social ul li').bind('touchstart', function () {
             $(".active").removeClass("active");
             $(this).addClass('active');
         });
@@ -319,13 +310,13 @@ jQuery(function($) {
     }
 
     /* ==================================================
-       Accordion
-       ================================================== */
+     Accordion
+     ================================================== */
 
-    BRUSHED.accordion = function() {
+    BRUSHED.accordion = function () {
         var accordion_trigger = $('.accordion-heading.accordionize');
 
-        accordion_trigger.delegate('.accordion-toggle', 'click', function(event) {
+        accordion_trigger.delegate('.accordion-toggle', 'click', function (event) {
             if ($(this).hasClass('active')) {
                 $(this).removeClass('active');
                 $(this).addClass('inactive');
@@ -340,13 +331,13 @@ jQuery(function($) {
     }
 
     /* ==================================================
-       Toggle
-       ================================================== */
+     Toggle
+     ================================================== */
 
-    BRUSHED.toggle = function() {
+    BRUSHED.toggle = function () {
         var accordion_trigger_toggle = $('.accordion-heading.togglize');
 
-        accordion_trigger_toggle.delegate('.accordion-toggle', 'click', function(event) {
+        accordion_trigger_toggle.delegate('.accordion-toggle', 'click', function (event) {
             if ($(this).hasClass('active')) {
                 $(this).removeClass('active');
                 $(this).addClass('inactive');
@@ -359,25 +350,25 @@ jQuery(function($) {
     }
 
     /* ==================================================
-       Tooltip
-       ================================================== */
+     Tooltip
+     ================================================== */
 
-    BRUSHED.toolTip = function() {
+    BRUSHED.toolTip = function () {
         $('a[data-toggle=tooltip]').tooltip();
     }
 
 
     /* ==================================================
-    	Init
-    	================================================== */
+     Init
+     ================================================== */
 
     BRUSHED.slider();
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         Modernizr.load([{
             test: Modernizr.placeholder,
             nope: './js/placeholder.js',
-            complete: function() {
+            complete: function () {
                 if (!Modernizr.placeholder) {
                     // Placeholders.init({
                     // live: true,
@@ -395,7 +386,7 @@ jQuery(function($) {
             showSplash: true,
             showPercentage: true,
             autoClose: true,
-            splashFunction: function() {
+            splashFunction: function () {
                 $('#circle').delay(250).animate({
                     'opacity': 1
                 }, 500, 'linear');
@@ -403,7 +394,7 @@ jQuery(function($) {
         });
 
         //初始化地图
-        BRUSHED.initMap = function() {
+        BRUSHED.initMap = function () {
             // 百度地图API功能
             ShowMap('114.199631,22.664393', '深圳市骊驰家具有限公司', '山塘工业区', '086-0755-89765854', '086-0755-89765854', '15');
 
@@ -416,7 +407,7 @@ jQuery(function($) {
                     data: {
                         ID: id
                     },
-                    success: function(data) {
+                    success: function (data) {
                         data = eval(data);
                         var length = data.length;
                         if (length > 0) {
@@ -434,7 +425,7 @@ jQuery(function($) {
                 var marker = new BMap.Marker(new BMap.Point(arrzuobiao[0], arrzuobiao[1]));
                 map.addOverlay(marker);
                 var infoWindow = new BMap.InfoWindow('<p style="color: #bf0008;font-size:14px;">' + name + '</p><p>地址：' + addrsee + '</p><p>电话：' + phone + '</p><p>传真：' + chuanzhen + '</p>');
-                marker.addEventListener("click", function() {
+                marker.addEventListener("click", function () {
                     this.openInfoWindow(infoWindow);
                 });
                 marker.openInfoWindow(infoWindow);
@@ -442,14 +433,14 @@ jQuery(function($) {
         }
 
 
-        BRUSHED.changeProduct = function() {
+        BRUSHED.changeProduct = function () {
 
             /*读取json*/
-            $.getJSON('./data/furniture.json', function(data) {
+            $.getJSON('./data/furniture.json', function (data) {
                 var _data = data;
                 readString("SOFA", 1);
                 /*点击事件*/
-                $('.sub-menu').children('li').on('click', function() {
+                $('.sub-menu').children('li').on('click', function () {
                     $('#thumbs').empty();
                     var furnitureName = $(this).parent().prev().text();
                     var enName;
@@ -510,7 +501,7 @@ jQuery(function($) {
 
             //读取EVENT
             var modalApp;
-            $.getJSON('./data/event.json', function(data) {
+            $.getJSON('./data/event.json', function (data) {
                 var _data = data;
                 // console.log(_data);
                 var app1 = new Vue({
@@ -519,7 +510,7 @@ jQuery(function($) {
                         events: _data
                     },
                     methods: {
-                        showEventModal: function(id) {
+                        showEventModal: function (id) {
                             app2.title = _data[id].title;
                             app2.img = _data[id].img;
                             app2.copy = _data[id].copy;
@@ -536,7 +527,7 @@ jQuery(function($) {
             });
 
             //读取新闻
-            $.getJSON('./data/news.json', function(data) {
+            $.getJSON('./data/news.json', function (data) {
                 var _data = data;
                 console.log(_data);
 
@@ -546,7 +537,7 @@ jQuery(function($) {
                         news: _data
                     },
                     methods: {
-                        showNewsModal: function(id) {
+                        showNewsModal: function (id) {
                             // console.log(id);
                             modalApp.title = _data[id].title;
                             modalApp.img = _data[id].img;
@@ -558,7 +549,6 @@ jQuery(function($) {
             });
 
         };
-
 
 
         BRUSHED.nav();
@@ -578,7 +568,7 @@ jQuery(function($) {
 
     });
 
-    $(window).resize(function() {
+    $(window).resize(function () {
         BRUSHED.mobileNav();
     });
 
