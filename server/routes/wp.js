9f6918d69ab6fe39870853065fa4ef785872381d
miller <miller.lmf@gmail.com>
2017-06-07 23:06:39 +0800

    update API

--- server/routes/wp.js
+++ server/routes/wp.js
@@ -1,34 +1,26 @@
 var express = require('express');
 var router = express.Router();
 var Wp = require("./../models/Wp");
-
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://miller:xiao@121.40.148.8:27017/mintWp');
+var db = mongoose.connection;
+// var db = mongoose.createConnection('mongodb://miller:xiao@121.40.148.8:27017/mintWp');
+// 链接错误
+db.on('error', function (error) {
+    console.log(error);
+});
 router.get('/', function (req, res) {
     res.send("wp");
 });
 
 router.get('/getDataList', function (req, res, next) {
-    Wp.getDataList(function (err, docs) {
-        res.send(docs);
+    Wp.find({}).sort({"id": -1}).limit(10).exec(function (err, docs) {
+        if (err) throw err;
+        // object of the user
+        var response = {status: 1, data: docs, errorDesc: err};
+        res.send(response);
     });
 });
-
-
-/*exports.doMovieAdd = function (req, res) {
- console.log(req.body.content);
- var json = req.body.content;
- // //var json = JSON.parse(req.body.content);
- if (json._id) {//update
-
- } else {//insert
- Wp.save(json, function (err) {
- if (err) {
- res.send({'success': false, 'err': err});
- } else {
- res.send({'success': true});
- }
- });
- }
- };*/
 
 module.exports = router;
 
